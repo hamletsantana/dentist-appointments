61f6cb1ebc39f4637e891f53dbe623d7b7d2b9c8
Hamlet Santana <danielhamlet123@gmail.com>
2024-04-10 01:19:52 -0500

    changes

--- src/Pages/Triaje/Procedimientos/Cirugias/cirugias.jsx
+++ src/Pages/Triaje/Procedimientos/Cirugias/cirugias.jsx
@@ -1,8 +1,66 @@
+import React, { useState, useEffect } from "react";
 import NavBar from "../../../../Components/NavBar/navBar";
-function Cirugias() {
-    return (
-        <NavBar />
-    );
+import "./cirugias.css"; // Import CSS for CirugiasPage
+
+const Cirugias = () => {
+  const [cirugiasData, setCirugiasData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(
+      "https://3lmv2y6pmb.execute-api.us-east-1.amazonaws.com/development/dentist-appointments-get-requests?type=cirugias"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setCirugiasData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
-  
-  export default Cirugias;
+
+  return (
+    <>
+      <NavBar></NavBar>
+      <div>
+        <h1>Cirugias Page</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Cédula</th>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>Fecha de Nacimiento</th>
+              <th>Dirección</th>
+              <th>Teléfono</th>
+              <th>Emergencia Médica</th>
+              {/* Add more table headers as needed */}
+            </tr>
+          </thead>
+          <tbody>
+            {cirugiasData.map((item, index) => (
+              <tr key={index}>
+                <td>{item.cedula.S}</td>
+                <td>{item.nombrePaciente.S}</td>
+                <td>{item.apellidoPaciente.S}</td>
+                <td>{item.edadPaciente.S}</td>
+                <td>{item.direccionPaciente.S}</td>
+                <td>{item.telefonoPaciente.S}</td>
+                <td>{item.emergenciaMedica.S}</td>
+                {/* Add more table cells as needed */}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default Cirugias;
